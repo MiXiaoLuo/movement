3bed8c6816acf16b10f6e7eb5e37d6c32bd70552
ZXY <15036168760@163.com>
2019-04-12 15:36:42 +0800

    123
    123

--- client/pages/activity/activity.js
+++ client/pages/activity/activity.js
@@ -5,7 +5,48 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    details: [
+      {
+        img: '../../images/activity-0.jpg',
+        title: '《夜的钢琴曲》—石进钢琴音乐会郑州站',
+        time: '2019-5-11',
+        address: '河南艺术中心',
+        tickets: '￥100',
+        residue: '356'
+      },
+      {
+        img: '../../images/activity-1.jpg',
+        title: '话剧《亲切的凶手》',
+        time: '2019.04.19 - 2019.05.18',
+        address: '丹尼斯大卫城',
+        tickets: '￥100',
+        residue: '486'
+      },
+      {
+        img: '../../images/activity-2.jpg',
+        title: '昆剧《赵氏孤儿》巡演',
+        time: '2019.05.16',
+        address: '河南艺术中心',
+        tickets: '￥50',
+        residue: '364'
+      },
+      {
+        img: '../../images/activity-3.jpg',
+        title: '昆剧《牡丹亭》巡演',
+        time: '2019.05.15',
+        address: '河南艺术中心',
+        tickets: '￥50',
+        residue: '423'
+      },
+      {
+        img: '../../images/activity-4.jpg',
+        title: '魅力古典音乐季 "今夜无人入睡"—意大利圣雷莫交响乐团歌剧歌曲音乐会',
+        time: '2019.05.14',
+        address: '河南艺术中心',
+        tickets: '￥80',
+        residue: '318'
+      }
+    ]
   },
 
   /**
@@ -62,5 +103,10 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  toDetails: function (e) {
+    wx.navigateTo({
+      url: '../a-details/a-details?id=' + e.currentTarget.id
+    })
   }
 })
--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -1,54 +1,114 @@
 //index.js
-//获取应用实例
-const app = getApp()
-
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+    imgUrls: [
+      '../../images/banner0.png',
+      '../../images/banner1.png',
+      '../../images/banner2.png',
+      '../../images/banner3.png',
+      '../../images/banner4.png'
+      ],
+    details: [
+        {
+          img: '../../images/banner0.png',
+          title:'千群健身',
+          time: '09:00-22:00',
+          address: '健康路168号2楼3楼(省体育场)',
+          tickets: '￥9.9',
+          residue: '56'
+        },
+        {
+          img: '../../images/banner1.png',
+          title: '乐动健身俱乐部',
+          time: '09:00-22:00',
+          address: '英才街国基生活区旗杆向南50米路西',
+          tickets: '￥9.9',
+          residue: '32'
+        },
+        {
+          img: '../../images/banner2.png',
+          title: '中州·沁雅',
+          time: '09:00-22:00',
+          address: '农业路71号',
+          tickets: '￥9.9',
+          residue: '63'
+        },
+        {
+          img: '../../images/banner3.png',
+          title: '莱喜健身俱乐部',
+          time: '09:00-22:00',
+          address: '航海路中州大道交汇处美食广场3楼(美景天城)',
+          tickets: '￥14.9',
+          residue: '47'
+        },
+        {
+          img: '../../images/banner4.png',
+          title: '拜逑台球会所',
+          time: '09:00-22:00',
+          address: '交通路与政通路交叉口向东300米路北B1楼',
+          tickets: '￥9.8',
+          residue: '30'
         }
-      })
-    }
+      ]
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
 })
